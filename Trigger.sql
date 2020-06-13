2de2f4af8ead5401c7455989cd3828e50671545a
darkshot <ameh46870@gmail.com>
2020-06-13 19:46:38 +0430

    trigger in place

--- Trigger.sql
+++ Trigger.sql
@@ -1,10 +1,16 @@
 
 CREATE TRIGGER ReciptContentCreate
 ON ReciptContent
-AFTER INSERT
+AFTER INSERT,DELETE
 AS
 BEGIN
     SET NOCOUNT ON;
     UPDATE Recipts Set 
-	TotalPrice=Select
+
+	Recipts.TotalPrice=content.totalprice
+	FROM (
+		SELECT SUM(price) as totalprice,ReciptId
+		FROM ReciptContent) AS content
+	WHERE 
+		content.ReciptId = Recipts.Id
 END
